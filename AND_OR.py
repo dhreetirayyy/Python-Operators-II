e73e7a96ce8c87d4a0f884625f9955260c212981
dhreetirayyy <dkumar.deo@gmail.com>
2025-03-19 16:50:47 -0700

    Learned AND & OR Operators

---
+++ AND_OR.py
@@ -0,0 +1,19 @@
+a = 10
+b = -10
+c = 0
+if a and b and c:
+    print("All the numbers have boolen values as true")
+else:
+    print("Atleast one boolen value is false")
+
+a = 10
+b = -10
+c = 0
+if a > 0 or b > 0:
+    print("Either of number should be greater than 0")
+else:
+    print("No number is greater than 0")
+if b > 0 or c > 0:
+    print("Either of number should be greater than 0")
+else:
+    print("No number is greater than 0")
